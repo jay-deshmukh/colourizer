054d33142fabd477570c62955488c70e53c4302b
Jay <jay>
2019-06-13 15:07:02 +0530

    created react app and added palette

---
+++ src/Paletee.js
@@ -0,0 +1,17 @@
+import React , { Component } from "react";
+
+class Paletee extends Component {
+  render(){
+    return(
+      <div className="Palette">
+        {/* TODO :: Add Navbar  */}
+        <div className="Palette-colours">
+          {/* TODO :: Add Bunch Of Colour Boxes */}
+        </div>
+        {/* TODO :: Add Footer  */}
+      </div>
+    )
+  }
+}
+
+export default Paletee;
